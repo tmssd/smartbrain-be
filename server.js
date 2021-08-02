9d78d2e808f1107f87dabca33b6a20bafa8307a8
tmssd <mail@tssrd.com>
2021-08-02 20:01:55 +0300

    update

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true,
-        // ssl: {
-        //     rejectUnauthorized: false
-        // },
+        // ssl: true,
+        ssl: {
+            rejectUnauthorized: false
+        },
     }
 });
 
